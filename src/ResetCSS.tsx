29531076611114bf3163593a6bddb8788cdcb312
Víctor Hugo Campos Vázquez <vcamposvazquez@gmail.com>
2021-07-16 10:58:36 +0200

    cards

--- src/ResetCSS.tsx
+++ src/ResetCSS.tsx
@@ -64,7 +64,7 @@ const ResetCSS = createGlobalStyle`
     box-sizing: border-box;
   }
   * {
-    font-family: '"Open Sans";
+    font-family: "Open Sans";
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
